d4997962b7fc877f006be71cc79c68b463909719
kbberker <kbbuk90@gmail.com>
2019-03-28 13:57:12 +0000

    Add FlightSearch page template

---
+++ src/FlightSearch.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class FlightSearch extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h1>FIND FLIGHTS!</h1>
+      </div>
+    );
+  }
+}
+
+export default FlightSearch;
