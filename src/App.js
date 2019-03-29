0b9c7ded5d1b0e1a79902d7780795748f93e2132
kbberker <kbbuk90@gmail.com>
2019-03-29 10:34:40 +0000

    Pick dep airport and setState for both dep and arr

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import FlightSearch from "./FlightSearch";
-
 import "./App.css";
 
 class App extends Component {
--- src/FlightSearch.js
+++ src/FlightSearch.js
@@ -1,11 +1,57 @@
 import React, { Component } from "react";
 
 class FlightSearch extends Component {
-  state = {};
+  state = {
+    departureAirport: "LGW",
+    arrivalAirport: "BCN"
+  };
+
+  handleDepartureChange = event => {
+    this.setState({
+      departureAirport: event.target.value,
+      arrivalAirport: event.target.value === "LGW" ? "BCN" : "LGW"
+    });
+  };
+
   render() {
     return (
       <div>
         <h1>FIND FLIGHTS!</h1>
+        <div>
+          <h4>Departure Airport: </h4>
+          <div>
+            <label>
+              <input
+                type="radio"
+                name="departureAirport"
+                value="LGW"
+                checked={this.state.departureAirport === "LGW"}
+                onChange={this.handleDepartureChange}
+              />
+              London Gatwick
+            </label>
+          </div>
+          <div>
+            <label>
+              <input
+                type="radio"
+                name="departureAirport"
+                value="BCN"
+                checked={this.state.departureAirport === "BCN"}
+                onChange={this.handleDepartureChange}
+              />
+              Barcelona
+            </label>
+          </div>
+        </div>
+        <div>
+          <h4>Destination:</h4>
+          {this.state.arrivalAirport === "LGW" ? (
+            <h5>London Gatwick</h5>
+          ) : (
+            <h5>Barcelona</h5>
+          )}
+        </div>
       </div>
     );
   }
